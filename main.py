3d64af72d67f817e297871a3fbdbeaa28c2d1588
Drake <drakec1997@gmail.com>
2021-04-25 11:54:34 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+# |========================================================|
+# |    Title:      The Sign out Front                      |
+# |    Author:     Drake G. Cummings                       |
+# |    Purpose:    Simulate advertisement efficiency       |
+# |    Date:       April 25th, 2021                        |
+# |========================================================|
