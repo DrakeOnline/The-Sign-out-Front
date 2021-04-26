91ea3b63c39502ac5418c86b44e35cb914539b5e
Drake <drakec1997@gmail.com>
2021-04-25 17:27:37 -0700

    Added compressed time cycle and Student class

---
+++ Student.py
@@ -0,0 +1,8 @@
+# |========================================================|
+# |    Title:      Student                                 |
+# |    Author:     Drake G. Cummings                       |
+# |    Purpose:    Simulate a Student                      |
+# |    Date:       April 25th, 2021                        |
+# |========================================================|
+
+class Student:
--- main.py
+++ main.py
@@ -8,39 +8,55 @@
 from LinkedList import LinkedList
 from os import system
 import time
+import datetime
 import math
 
 
+# |=========================TIME CONVERSIONS===============================|
+# |  Sign cycle:                   20 s        ->  20,000 ms               |
+# |  Student visibility time:      60 s        ->  60,000 ms               |
+# |  Simulation period (1 week):   604,800 s   ->  604,800,000 ms          |
+# |=========================AFTER COMPRESSION==============================|
+# |  Sign cycle:                   20,000 ms       ->  2 ms                |
+# |  Student visibility time:      60,000 ms       ->  6 ms                |
+# |  Simulation period (1 week):   604,800,000 ms  ->  60,480 ms           |
+# |==============================RESULTS===================================|
+# |  Maximum simulation time: 60.48 seconds ~ 1 minute                     |
+# |========================================================================|
+
+
+# |=========================MAKE SIGNS===============================|
 # Create circular list for signs
 signs = LinkedList("Ad 1")
 
 # Fill linkedlist with 20 ads
-for x in range(2,21):
+for x in range(2, 21):
     signs.append_right(f"Ad {x}")
 
 # Make circular
 signs.get_tail().next = signs.head
 
-# Change sign every 20 seconds
-startTime = math.floor(time.time())
+
+# |=========================SIGN TIMER===============================|
+startTime = int(round(time.time() * 1000))
 prevTime = 0
+elapsedTime = 0
 currentSign = signs.head
 
-# Continuous timer
-while True:
-    # Rund time down
-    now = math.floor(time.time())
+# Counts down a week's simulated time
+while elapsedTime < 60480:
+    # Millisecond timer
+    now = int(round(time.time() * 1000))
 
     # How much time has passed
-    elapsedTime = now - startTime
+    elapsedTime = (now - startTime)
 
-    # check if 2 seconds has passed
-    if elapsedTime % 1 == 0 and elapsedTime != prevTime:
+    # Check if 2 milliseconds has passed
+    # To change sign every 2 milliseconds
+    if elapsedTime % 2 == 0 and elapsedTime != prevTime:
         # If it had already run this cycle
         prevTime = elapsedTime
         system("cls")
+        print("")
         print(f"{currentSign.value}")
         currentSign = currentSign.next
-
-    # Try to limit cycle count
-    time.sleep(0.01)
